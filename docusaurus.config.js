768fb8cbdb0430edb37ce4df220227ed8f5f2610
Alex Ganose <utf@users.noreply.github.com>
2022-11-08 23:16:12 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/virtualatoms/handbook',
+          editUrl: 'https://github.com/virtualatoms/handbook/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
